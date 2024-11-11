ec74f9104f779acaf8b06a612b14e105f17833d0
lazely <lz0136@naver.com>
2024-11-12 01:54:02 +0900

    ✨model utils → torch.vision기반으로 일단 옮겨서 사용할 수 있도록 Fixes #12

--- src/models/model_utils.py
+++ src/models/model_utils.py
@@ -2,21 +2,24 @@ import timm
 import torch.nn as nn
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 import torch.optim as optim
+from torchvision import models
 
 def get_criterion(criterion_name):
     if criterion_name == "CrossEntropyLoss":
         return nn.CrossEntropyLoss()
+    elif criterion_name == "BCE":
+        return nn.BCEWithLogitsLoss()
     else:
         raise ValueError(f"Unsupported criterion: {criterion_name}")
 
 def get_optimizer(config, model_parameters):
     optimizer_name = config['training']['optimizer']
     if optimizer_name == "Adam":
-        return optim.Adam(model_parameters, lr=config['training']['learning_rate'], 
-                          weight_decay=config['training']['weight_decay'])
+        return optim.Adam(model_parameters, lr=config['optimizer']['learning_rate'], 
+                          weight_decay=config['optimizer']['weight_decay'])
     elif optimizer_name == "SGD":
-        return optim.SGD(model_parameters, lr=config['training']['learning_rate'], 
-                         momentum=0.9, weight_decay=config['training']['weight_decay'])
+        return optim.SGD(model_parameters, lr=config['optimizer']['learning_rate'], 
+                         momentum=config['optimizer']['momentum'], weight_decay=config['optimizer']['weight_decay'])
     else:
         raise ValueError(f"Unsupported optimizer: {optimizer_name}")
     
@@ -37,28 +40,16 @@ def get_lr_scheduler(optimizer, scheduler_config):
 
 # timm 라이브러리에서 모델 불러오기
 def get_model(config):
-    model_config = config['model']
-    num_classes = model_config['num_classes']
+    model_name = config['model']['name']
+    num_classes = len(config['classes'])
     
-    model_config_name = model_config['name']
 
-    # 모델 간소화 시 모델 전체 이름 매핑
-    model_mapping = {
-        "resnet":"resnet50",
-        "ViT":"vit_base_patch16_224.augreg_in21k",
-        "ConvN":"convnext_base",
-        "eff3":"efficientnet_b3",
-        "eff4":"efficientnet_b4",
-        "eff5":"efficientnet_b5",
-        "eff6":"efficientnet_b6",
-        "eff7":"efficientnet_b7",
-        "densenet": "densenet121"
-    }
+    if model_name == 'fcn_50':
+        model = models.segmentation.fcn_resnet50(pretrained=config['model']['pretrained'])
+        model.classifier[4] = nn.Conv2d(512, num_classes, kernel_size=1)
 
     # 매핑된 모델 이름 가져오기, 없으면 원래 이름 사용
-    model_name = model_mapping.get(model_config_name, model_config_name)
+    #model_name = model_mapping.get(model_config_name, model_config_name)
 
-    # 모델 생성
-    model = timm.create_model(model_name, pretrained=config['model']['pretrained'], num_classes=num_classes)
 
     return model
