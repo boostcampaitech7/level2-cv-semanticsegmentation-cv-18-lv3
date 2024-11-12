ccc105a60e0b1ee03ad9341acf5e81491e2d9cc0
kkh090 <kihoon090@gmail.com>
2024-11-12 05:14:56 +0000

    ♻️ [REFACTOR] inference.py에 있던 rle encoder & decoder 개별 파일로 옮기기 Fixes #24

--- src/inference.py
+++ src/inference.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 from src.models.model_utils import get_model
 from src.utils.data_loaders import get_test_loaders
+from src.utils.rle_convert import encode_mask_to_rle
 
 def run(config):
     thr = config['thr']
@@ -56,28 +57,3 @@ def run(config):
         "rle": rles,
     })
     df.to_csv("output.csv", index=False)
-    
-def encode_mask_to_rle(mask):
-    '''
-    mask: numpy array binary mask 
-    1 - mask 
-    0 - background
-    Returns encoded run length 
-    '''
-    pixels = mask.flatten()
-    pixels = np.concatenate([[0], pixels, [0]])
-    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
-    runs[1::2] -= runs[::2]
-    return ' '.join(str(x) for x in runs)
-
-def decode_rle_to_mask(rle, height, width):
-    s = rle.split()
-    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
-    starts -= 1
-    ends = starts + lengths
-    img = np.zeros(height * width, dtype=np.uint8)
-    
-    for lo, hi in zip(starts, ends):
-        img[lo:hi] = 1
-    
-    return img.reshape(height, width)
