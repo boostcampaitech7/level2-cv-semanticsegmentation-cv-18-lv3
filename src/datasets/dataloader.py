ac63fdcdbbb142a05dbbb7dcf3407bc618295153
lazely <lz0136@naver.com>
2024-11-12 13:08:40 +0900

    🐛 dataloader 추가 수정

--- src/datasets/dataloader.py
+++ src/datasets/dataloader.py
@@ -5,7 +5,7 @@ import torch
 from torch.utils.data import Dataset, DataLoader, Subset
 from typing import Dict, Any, Tuple
 import albumentations as A
-from utils.datasets import XRayDataset, get_transform
+from datasets.dataset import XRayDataset
 
 def get_data_loaders(config: Dict[str, Any]) -> Tuple[DataLoader, DataLoader]:
     batch_size = config['train']['batch_size']
@@ -30,14 +30,14 @@ def get_data_loaders(config: Dict[str, Any]) -> Tuple[DataLoader, DataLoader]:
 
     train_loader = DataLoader(
         train_dataset,
-        batch_size=config['train']['batch_size'],
+        batch_size=batch_size,
         shuffle=True,
         num_workers=8,
         drop_last=True
     )
     val_loader = DataLoader(
         val_dataset,
-        batch_size=config['train']['batch_size'],
+        batch_size=batch_size,
         shuffle=False,
         num_workers=8,
         drop_last=False
@@ -51,10 +51,10 @@ def get_inference_loaders(config: Dict[str, Any]) -> DataLoader:
     tf = A.Resize(512, 512)
     
     inference_dataset = XRayDataset(
-        image_root=config['paths']['test_image_root']
-        label_root=config['paths']['test_label_root']
-        classes=config['classes']
-        mode='test'
+        image_root=config['paths']['test_image_root'],
+        label_root=config['paths']['test_label_root'],
+        classes=config['classes'],
+        mode='test',
         transforms=tf
     )
     
