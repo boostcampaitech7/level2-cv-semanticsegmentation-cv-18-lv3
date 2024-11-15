e4a42d39c2cbe1a357e6d8ab5036d9723ef59390
kkh090 <kihoon090@gmail.com>
2024-11-15 04:30:06 +0000

    ♻️ REFACTOR: wandb logger 클래스화

--- src/utils/wandb_logger.py
+++ src/utils/wandb_logger.py
@@ -1,63 +1,63 @@
 import wandb
 import datetime
 from typing import Dict, Any
-
-def init_wandb(config: Dict[str, Any]) -> None:
-
-    current_date = datetime.datetime.now().strftime("%Y%m%d")
-    model_name = config['model']['name']
-    user_name = config['wandb']['user_name']
-    team_name = config['wandb']['team_name']
-
-    project_name = f"{model_name}"
-    
-    run_name = f"{model_name}_{user_name}_{current_date}"
-    
-    # wandb config 
-    wandb_config = {
-        "model_name": config['model']['name'],
-        "batch_size": config['train']['batch_size'],
-        "learning_rate": config['train']['optimizer']['config']['lr'],
-        "optimizer": config['train']['optimizer']['name'],
-        "weight_decay": config['train']['optimizer']['config']['weight_decay'], 
-        "lr_scheduler": config['train']['lr_scheduler']['name'],
-        "owner" : user_name #누가 돌렸는 지 정보 추가 
-    }
-    
-    # wandb initialize 
-    try:
-        wandb.init(project=project_name, entity=team_name, config=wandb_config,  name=run_name)
-    except Exception as e:
-        print(f"Error during W&B initialization: {e}")
-
-def log_metrics(epoch: int, train_loss: float, val_loss: float = None, val_metric: float = None) -> None:
-
-    metrics = {
-        "epoch": epoch,
-        "train_loss": train_loss
-    }
-    if val_loss is not None:
-        metrics["val_loss"] = val_loss
-    if val_metric is not None:
-        metrics["val_metric"] = val_metric
-    try:
-        wandb.log(metrics)
-    except Exception as e:
-        print(f"Error recording W&B logs: {e}")
-
-def finish_wandb():
-    try:
-        wandb.finish()
-    except Exception as e:
-        print(f"Error during wandb finish: {e}")
-        
         
 class WandbLogger():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, config: dict[str, any]) -> None:
+        if 'wandb' in config.keys(): # wandb_status off : 0, on : 1
+            self.init_wandb(config)
+            self.wandb_status = True
+        else:
+            print("wandb not found in config: running train without logger")
+            self.wandb_status = False
 
-    def log_metrics(self) -> None:
-        pass
-    
-    def finish_wandb():
-        pas
+    @staticmethod
+    def init_wandb(config: dict[str, Any]) -> None:
+        current_date = datetime.datetime.now().strftime("%Y%m%d")
+        model_name = config['model']['name']
+        user_name = config['wandb']['user_name']
+        team_name = config['wandb']['team_name']
+
+        project_name = f"dev_{config['developer']}" if user_name == 'dev' else f"{model_name}"
+        
+        run_name = f"{model_name}_{user_name}_{current_date}"
+        
+        # wandb config 
+        wandb_config = {
+            "model_name": config['model']['name'],
+            "batch_size": config['train']['batch_size'],
+            "learning_rate": config['train']['optimizer']['config']['lr'],
+            "optimizer": config['train']['optimizer']['name'],
+            "weight_decay": config['train']['optimizer']['config']['weight_decay'],
+            "lr_scheduler": config['train']['lr_scheduler']['name'],
+            "owner" : user_name #누가 돌렸는 지 정보 추가
+        }
+        
+        # wandb initialize 
+        try:
+            wandb.init(project=project_name, entity=team_name, config=wandb_config,  name=run_name)
+        except Exception as e:
+            print(f"Error during W&B initialization: {e}")
+
+    def log_metrics(self, epoch: int, train_loss: float, val_loss: float = None, val_metric: float = None) -> None:
+        if self.wandb_status:
+            metrics = {
+                "epoch": epoch,
+                "train_loss": train_loss
+            }
+            if val_loss is not None:
+                metrics["val_loss"] = val_loss
+            if val_metric is not None:
+                metrics["val_metric"] = val_metric
+            
+            try:
+                wandb.log(metrics)
+            except Exception as e:
+                print(f"Error recording W&B logs: {e}")
+        
+    def finish_wandb(self) -> None:
+        if self.wandb_status:
+            try:
+                wandb.finish()
+            except Exception as e:
+                print(f"Error during wandb finish: {e}")
