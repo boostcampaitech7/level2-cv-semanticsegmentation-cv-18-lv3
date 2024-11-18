602281fde90928f331f6f2a358d97f631aecb013
HAEGANG LEE <46679187+lazely@users.noreply.github.com>
2024-11-18 12:16:35 +0900

    ♻️ REFACTOR: loss.py 분리 ✨FEAT: 새로운 loss 추가 (#77)
    * ♻️ REFACTOR: loss.py 분리
    Fixes #70
    
    * ✨ FEAT: 6강에 나온 unet+++ loss 추가

--- src/models/model_utils.py
+++ src/models/model_utils.py
@@ -7,66 +7,21 @@ import torch.optim as optim
 from torchvision import models
 from typing import Any, Dict, Optional
 import segmentation_models_pytorch as smp
-import torch.nn.functional as F
+
 from .unet import UNetResNet34 
 from .SAM2UNet import SAM2UNet
 
-def calc_loss_bce_dice(pred, target, bce_weight=0.5):
-    bce = F.binary_cross_entropy_with_logits(pred, target)
-    pred = F.sigmoid(pred)
-    dice = dice_loss(pred, target)
-    loss = bce * bce_weight + dice * (1 - bce_weight)
-    return loss
-
-def dice_loss(pred, target, smooth = 1.):
-    pred = pred.contiguous()
-    target = target.contiguous()   
-    intersection = (pred * target).sum(dim=2).sum(dim=2)
-    loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) +   target.sum(dim=2).sum(dim=2) + smooth)))
-    return loss.mean()
-
-def focal_loss(inputs, targets, alpha=.25, gamma=2) : 
-    BCE = F.binary_cross_entropy_with_logits(inputs, targets)
-    BCE_EXP = torch.exp(-BCE)
-    loss = alpha * (1-BCE_EXP)**gamma * BCE
-    return loss
-
-def focal_dice_loss(pred=None, target=None, focal_weight = 0.5):
-    focal = focal_loss(pred, target)
-    pred = F.sigmoid(pred)
-    dice = dice_loss(pred, target)
-    loss = focal * focal_weight + dice * (1 - focal_weight)
-    return loss
-
-    
-
-def structure_loss(pred, target):
-    weit = 1 + 5 * torch.abs(F.avg_pool2d(target, kernel_size=31, stride=1, padding=15) - target)
-    wbce = F.binary_cross_entropy_with_logits(pred, target, reduction='none')
-    wbce = (weit * wbce).sum(dim=(2, 3)) / weit.sum(dim=(2, 3))
-    pred = torch.sigmoid(pred)
-    inter = ((pred * target) * weit).sum(dim=(2, 3))
-    union = ((pred + target) * weit).sum(dim=(2, 3))
-    wiou = 1 - (inter + 1) / (union - inter + 1)
-    return (wbce + wiou).mean()
-
-def multiscale_structure_loss(pred, target):
-    loss = 0
-    pred0, pred1, pred2 = pred
-    loss0 = structure_loss(pred0, target)
-    loss1 = structure_loss(pred1, target)
-    loss2 = structure_loss(pred2, target)
-    loss = loss0 + loss1 + loss2
-    return loss
+from ..utils.loss import *
 
 def get_criterion(criterion_name: str) -> nn.Module:
     criterions = {
-        'CrossEntropyLoss': nn.CrossEntropyLoss(),
-        'BCEWithLogitsLoss': nn.BCEWithLogitsLoss(),
+        'CrossEntropy': cross_entropy_loss,
+        'bce': bce_loss,
         'bce+dice': calc_loss_bce_dice,
-        'dice' : dice_loss,
-        'StructureLoss' : multiscale_structure_loss,
-        'focal+dice' : focal_dice_loss 
+        'dice': dice_loss,
+        'StructureLoss': multiscale_structure_loss,
+        'focal+dice': focal_dice_loss,
+        'unet3p': unet3p_loss
     }
     if criterion_name in criterions:
         return criterions[criterion_name]
