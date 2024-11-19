b5391e4d02873706ddce74fe91abcdc22b01e096
heejin Jang <105128163+heeejini@users.noreply.github.com>
2024-11-19 10:08:56 +0900

    ✨ FEAT: 학습이 끝났을 시에 slack 알람 보내기 (#80)
    * ✨ FEAT: 학습이 끝났을 시에 slack 알람 보내기
    
    * ✨ FEAT: slack alarm 기능
    
    * 🔥 REMOVE : base.yaml webhook_url 삭제

--- src/train.py
+++ src/train.py
@@ -6,6 +6,8 @@ import numpy as np
 import datetime
 import uuid
 import os
+import requests
+from src.utils.alert import send_slack_notification
 from src.utils.trainer import train_one_epoch, validate, save_model
 from src.datasets.dataloader import get_data_loaders
 from src.utils.metrics import get_metric_function
@@ -94,4 +96,12 @@ def run(config: Dict[str, Any], resume: bool, pth_path: str, dev: bool) -> float
             break
     
     wandb.finish_wandb()
+    slack_webhook_url = config['webhook']['url']
+    message  = f"Training is done!\nBest val metric :{best_val_metric :.4f}"
+    
+    try:
+        send_slack_notification(config, slack_webhook_url, message)
+        print(f"slack 알람 성공")
+    except Exception as e:
+        print(f"Slack 알림 전송 실패: {e}")
     return best_val_metric
