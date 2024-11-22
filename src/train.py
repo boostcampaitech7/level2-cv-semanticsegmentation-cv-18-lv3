ff84f3b1ab5258f43338d8d2da438bc672e82400
heejin Jang <105128163+heeejini@users.noreply.github.com>
2024-11-22 10:12:20 +0900

    ♻️ REFACTOR: train.py 와 slack 알람 분리 (#84)
    * ♻️ REFACTOR: train.py 와 slack 알람 분리
    
    * ♻️ REFACTOR: slack 알람 기본 default 설정 및 webhook 주소 변경

--- src/train.py
+++ src/train.py
@@ -16,7 +16,7 @@ from src.utils.logger import WandbLogger, save_config
 from typing import Any, Dict
 
 
-def run(config: Dict[str, Any], resume: bool, pth_path: str, dev: bool) -> float:
+def run(config: Dict[str, Any], resume: bool, pth_path: str, dev: bool, webhook: bool) -> float:
     model_name = config['model']['name']
     threshold = config['train']['threshold']
 
@@ -96,12 +96,8 @@ def run(config: Dict[str, Any], resume: bool, pth_path: str, dev: bool) -> float
             break
     
     wandb.finish_wandb()
-    slack_webhook_url = config['webhook']['url']
-    message  = f"Training is done!\nBest val metric :{best_val_metric :.4f}"
     
-    try:
-        send_slack_notification(config, slack_webhook_url, message)
-        print(f"slack 알람 성공")
-    except Exception as e:
-        print(f"Slack 알림 전송 실패: {e}")
+    if webhook :
+        send_slack_notification(config, best_val_metric)
+
     return best_val_metric
--- src/utils/alert.py
+++ src/utils/alert.py
@@ -1,18 +1,18 @@
 import requests
-import datetime
 from typing import Dict, Any
 
-def send_slack_notification(config: Dict[str, Any], webhook_url: str, message: str) -> None:
-  
+def send_slack_notification(config: Dict[str, Any], best_val_metric: float) -> None:
+    slack_webhook_url = config['webhook']['url']
     model_name = config['model']['name']
     user_name = config['wandb']['user_name']
     
+    message  = f"Training is done!\nBest val metric :{best_val_metric :.4f}"
     slack_message = f"{message}\nModel Name: {model_name}\nUser Name: {user_name}"
     
     payload = {"text": slack_message}
     
     try:
-        response = requests.post(webhook_url, json=payload, timeout=10)
-        response.raise_for_status()  # HTTP 에러 발생 시 예외 발생
+        response = requests.post(slack_webhook_url, json=payload, timeout=10)
+        response.raise_for_status()  
     except requests.exceptions.RequestException as e:
         raise ValueError(f"Slack webhook 요청 실패: {e}")
